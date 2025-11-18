f9f39b58b519b57795b8cbae818a69387c62c70f
Guilherme Torres <ogtorresdev@gmail.com>
2025-11-17 22:36:46 -0300

    initial commit

---
+++ todo-macos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo-macos
+//
+//  Created by Guilherme Torres Vanderlei on 17/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo-macos/todo_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_macosApp.swift
+//  todo-macos
+//
+//  Created by Guilherme Torres Vanderlei on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct todo_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
